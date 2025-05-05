5d201bd97c7a6e845103bf2548df6a554729090e
Angel Rueda <adruedaq@sanmateo.edu.co>
2025-05-05 11:39:37 -0500

    Vlidar la sesion de inicio

--- Vista/pagina_principal.php
+++ Vista/pagina_principal.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+if (!isset($_SESSION['id_usuario'])) {
+    header("Location: Login.php");
+    exit();
+}
+
+$sesion_activa = isset($_SESSION['id_usuario']);
+?>
+
+
 <!DOCTYPE html>
 <head>
     <title>
